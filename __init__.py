7a6e31dc3d3fdc8a910cddf7a3dfd0a443c0c0f9
Joshua Infante <infante.joshua1994@gmail.com>
2023-04-27 16:26:54 -0700

    init creates __data__ in home directory

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
-from src import *
-from __data__ import *
+from os import listdir, mkdir
+from os.path import join
+from pathlib import Path
+
+
+ddir = '__data__'
+hdir = Path.home()
+pdir = join(hdir, ddir)
+if ddir in listdir(hdir): pass
+else: mkdir(pdir)
